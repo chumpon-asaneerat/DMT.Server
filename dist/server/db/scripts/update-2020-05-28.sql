3b7afa7b4b78e7064cf5ae3143ecd17eb5a444b6
hassakorn <hassakorn@vrsstudio.com>
2020-05-31 18:29:45 +0700

    Update 2020-05-31 #1 - Update from K.Tee for test using vscode.

---
+++ dist/server/db/scripts/update-2020-05-28.sql
@@ -0,0 +1,12 @@
+/*********** Script Update Date: 2020-05-28  ***********/
+
+-- TEST Script Table
+
+
+/*********** Script Update Date: 2020-05-28  ***********/
+
+-- TEST Script function
+
+
+/*********** Script Update Date: 2020-05-28  ***********/
+-- TEST Script function
