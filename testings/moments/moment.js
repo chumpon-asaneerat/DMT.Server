144bcf6a5f4ba8861df3b781abf267b0ed1c412e
chumpon-asaneerat <chumponsenate@gmail.com>
2020-10-18 22:00:28 +0700

    Update 2020-10-18 #3 - Update moment.js file for test date format and conversion #2.

--- testings/moments/moment.js
+++ testings/moments/moment.js
@@ -2,10 +2,12 @@ const moment = require('moment')
 
 // The moment custom date formats.
 const dateFormats = [
-    'YYYY-MM-DD HH.mm.ss.SSSZ',
-    'YYYY/MM/DD HH.mm.ss.SSSZ',
-    'YYYY-MM-DD HH:mm:ss.SSSZ',
-    'YYYY/MM/DD HH:mm:ss.SSSZ',
+    // Json Object Format Styles.
+    'YYYY-MM-DDTHH.mm.ss.SSSZ',
+    'YYYY/MM/DDTHH.mm.ss.SSSZ',
+    'YYYY-MM-DDTHH:mm:ss.SSSZ',
+    'YYYY/MM/DDTHH:mm:ss.SSSZ',
+    // SQL Server Format Styles.
     'YYYY-MM-DD HH.mm.ss.SSS',
     'YYYY/MM/DD HH.mm.ss.SSS',
     'YYYY-MM-DD HH:mm:ss.SSS',
@@ -15,50 +17,50 @@ const dateFormats = [
 const formatDateTime = (value) => {
     let ret = null;
     try {
-        let dt = moment(value, dateFormats, true).local();
-        //ret = (dt.isValid()) ? new Date(dt.utc()) : null;
-        let isValid = dt.isValid();
-        ret = (isValid) ? dt.toDate() : null;
+        let mObj = moment(value, dateFormats);
+        let isValid = mObj.isValid();
+        let dt = (isValid) ? dt.toDate() : null;
         // fixed timezone offset (need to check if has problem)
-        if (null !== ret) {
-            ret = new Date(ret.getTime() - (ret.getTimezoneOffset() * 60 * 1000))
+        if (null !== dt) {
+            let time = dt.getTime();
+            let tz = dt.getTimezoneOffset() * 60 * 1000;
+            ret = new Date(time + tz);
         }
         else {
+            console.log('Invalid date fomat : ', value);
             ret = new Date(value);
         }
-        //console.log('OTHER DATE (try to used moment.js):', ret);
     }
     catch (ex) {
         console.log(ex);
-        console.log('OTHER DATE (try to used moment.js): failed.');
     }
 
     return ret;
 }
 
-//let value = '2020-10-09'; // OK
-let value = '2020-10-09T07:00:00.511Z'; // OK due to Z is utc time.
-//let value = '2020-10-15T20:00:02.000+07:00'; // OK
+//let value = '2020-10-09'; // Json Date OK
+let value = '2020-10-09T07:00:00.511Z'; // Json Date OK due to Z is utc time.
+//let value = '2020-10-15T20:00:02.342+07:00'; // Json Date OK
+//let value = '2020/10/15 20:00:02.342'; // SQL Style OK
 
-let mObj = moment(value, dateFormats)
-//let mObj = moment(value)
-//let mObj = moment(value, dateFormats, false).local();
-
-console.log(mObj)
+//let mObj = moment(value, dateFormats)
+let mObj = moment(value)
+console.log('local datetime:', mObj)
 
 let isValid = mObj.isValid();
 console.log(isValid)
 
 let dt = mObj.toDate();
-console.log(dt)
+console.log('local datetime: ', dt)
 
-let time = dt.getTime()
+let time = new Date(dt.toUTCString()).getTime()
 let tz = dt.getTimezoneOffset() * 60 * 1000;
 console.log(time)
 console.log(tz)
 
-let dt2 = new Date(time + tz)
+let dt2 = new Date(time - tz)
 console.log(dt2)
+console.log(dt2.toJSON())
 
 let dt3 = new Date(Date.parse(value))
 console.log(dt3.toJSON())
