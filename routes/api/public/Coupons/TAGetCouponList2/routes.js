46f13d7e3854cdacd687c67d2b71cd69d531ddf0
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-18 00:43:13 +0700

    Update 2021-01-18 #3 - Update TA.Coupon.rest   - Add example for call api /api/users/coupons/getlist2 PASSED.

---
+++ routes/api/public/Coupons/TAGetCouponList2/routes.js
@@ -0,0 +1,80 @@
+//#region requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+
+const sqldb = require(path.join(nlib.paths.root, 'TAxTOD.db'));
+const dbutils = require(path.join(rootPath, 'dmt', 'utils', 'db-utils')).DbUtils;
+
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+// static class.
+const api = class { }
+
+//#region Implement - TAGetCouponList
+
+api.TAGetCouponList2 = class {
+    static prepare(req, res) {
+        let params = WebServer.parseReq(req).data;
+        if (params.userid == '') 
+        {
+            params.userid = null
+        }
+
+        if (params.transactiontype == '') 
+        {
+            params.transactiontype = null
+        }
+
+        if (params.coupontype == '') 
+        {
+            params.coupontype = null
+        }
+        //console.log('TA coupon getlist TSBId:', params.tsbid)
+
+        return params;
+    }
+    static async call(db, params) { 
+        return db.TAGetCouponList2(params)
+    }
+    static parse(db, data, callback) {
+        let result = dbutils.validate(db, data)
+        // execute callback
+        if (callback) callback(result)
+    }
+    static entry(req, res) {
+        let ref = api.TAGetCouponList2
+        let db = new sqldb()
+        let params = ref.prepare(req, res)
+        let fn = async () => { return ref.call(db, params) }
+        dbutils.exec(db, fn).then(data => {
+            ref.parse(db, data, (result) => {
+                WebServer.sendJson(req, res, result)
+            });
+        })
+    }
+}
+
+//#endregion
+
+router.all('/', api.TAGetCouponList2.entry)
+
+const init_routes = (svr) => {
+    svr.route('/api/users/coupons/getlist2', router);
+    //svr.route('/api/users/coupons/sold', router);
+
+    //svr.route('/api/rto/coupons/sold', router);
+    //svr.route('/api/rto/coupons/search', router);
+
+    // Attendance login/logout (Used later for TOD)
+    //svr.route('/api/rto/attendances/begin', router);
+    //svr.route('/api/rto/attendances/end', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
+
