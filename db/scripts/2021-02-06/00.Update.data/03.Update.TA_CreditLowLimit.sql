cecf4608e1725da1d645d7e072455f18b1fbaebe
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-06 17:09:01 +0700

    Update 2021-02-06 #3 - Add script   - Update TSBId on TA_CreditLowLimit table. - re-generate db script for 2021-02-06

---
+++ db/scripts/2021-02-06/00.Update.data/03.Update.TA_CreditLowLimit.sql
@@ -0,0 +1,11 @@
+UPDATE TA_CreditLowLimit
+   SET TSBId = '01'
+ WHERE TSBId = '311'
+
+GO
+
+UPDATE TA_CreditLowLimit
+   SET TSBId = '09'
+ WHERE TSBId = '319'
+
+GO
--- dist/server/db/scripts/update-2021-02-06.sql
+++ dist/server/db/scripts/update-2021-02-06.sql
@@ -79,3 +79,17 @@ UPDATE Lane
 
 GO
 
+
+/*********** Script Update Date: 2021-02-06  ***********/
+UPDATE TA_CreditLowLimit
+   SET TSBId = '01'
+ WHERE TSBId = '311'
+
+GO
+
+UPDATE TA_CreditLowLimit
+   SET TSBId = '09'
+ WHERE TSBId = '319'
+
+GO
+
