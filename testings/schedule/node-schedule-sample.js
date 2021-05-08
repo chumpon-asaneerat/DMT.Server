0c51c07b50d328c32fd436d8411d2d4b8b45d122
chumpon-asaneerat <chumponsenate@gmail.com>
2021-05-08 16:54:03 +0700

    Update 2021-05-08 #4 - Update ftp and file sync #4.

--- testings/schedule/node-schedule-sample.js
+++ testings/schedule/node-schedule-sample.js
@@ -4,6 +4,7 @@ const schedule = require('node-schedule');
 const rule = new schedule.RecurrenceRule();
 rule.second = 3;
 
-const job = schedule.scheduleJob(rule, function(){
+// see https://crontab.guru/ for example of cron format.
+const job = schedule.scheduleJob(rule, function() {
   console.log('The answer to life, the universe, and everything!');
 });
