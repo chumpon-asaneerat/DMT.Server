62795f661f5cea4bf98ae38f897fdfc14b410b74
chumpon-asaneerat <chumponsenate@gmail.com>
2021-06-22 16:22:33 +0700

    Update 2021-06-22 #1 - rename today.txt to history.txt - set node.js app version to v1.0.0 build 349. - add app version in logger file when start.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const path = require("path");
 //const nlib = require("./nlib/nlib");
 const WebServer = require('./nlib/nlib-express');
+// init logger
+const logger = require('./nlib/nlib-logger').logger;
+
 const fileSyncService = require('./dmt/filesync/filesync');
 const SFTPService = require('./dmt/ftp/nftp');
 
+// write app version to log
+logger.info('start TA Server v1.0..0 build 349 update 2021-05-20 16:20');
+
+// start web server
 const wsvr = new WebServer();
 wsvr.listen();
 
