fa6f9bbbcc4d1261760f6269580e757151a1d11a
chumpon-asaneerat <chumponsenate@gmail.com>
2021-09-21 17:47:11 +0700

    Update 2021-09-21 #2 - Update app version.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fileSyncService = require('./dmt/filesync/filesync');
 const SFTPService = require('./dmt/ftp/nftp');
 
 // write app version to log
-logger.info('start TA Server v1.0..0 build 352 update 2021-06-29 04:30');
+logger.info('start TA Server v1.0..0 build 360 update 2021-09-21 17:45');
 
 // start web server
 const wsvr = new WebServer();
