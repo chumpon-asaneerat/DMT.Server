bae9797412ea24afd7e5ba95c31a5761c93e7dfd
hassakorn <hassakorn@vrsstudio.com>
2022-10-30 10:32:20 +0700

    ADD SP for Edit Coupon Serial No

---
+++ routes/api/public/Coupons/TA_EditSoldCoupon/routes.js
@@ -0,0 +1,60 @@
+//#region requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+
+const sqldb = require(path.join(nlib.paths.root, 'TAxTOD.db'));
+const dbutils = require(path.join(rootPath, 'dmt', 'utils', 'db-utils')).DbUtils;
+
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+// static class.
+const api = class { }
+
+//#region Implement - TA_EditSoldCoupon
+
+api.TA_EditSoldCoupon = class {
+    static prepare(req, res) {
+        let params = WebServer.parseReq(req).data
+        
+             
+
+       // console.log('Coupons search sapitemcode:', params.SAPItemCode)
+
+        return params
+    }
+    static async call(db, params) { 
+        return db.TA_EditSoldCoupon(params)
+    }
+    static parse(db, data, callback) {
+        let result = dbutils.validate(db, data)
+        // execute callback
+        if (callback) callback(result)
+    }
+    static entry(req, res) {
+        let ref = api.TA_EditSoldCoupon
+        let db = new sqldb()
+        let params = ref.prepare(req, res)
+        let fn = async () => { return ref.call(db, params) }
+        dbutils.exec(db, fn).then(data => {
+            ref.parse(db, data, (result) => {
+                WebServer.sendJson(req, res, result)
+            });
+        })
+    }
+}
+
+//#endregion
+
+router.all('/', api.TA_EditSoldCoupon.entry)
+
+const init_routes = (svr) => {
+    svr.route('/api/TA/coupons/editserialno', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
