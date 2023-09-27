97e219276d03024acb0b99a178c37b87ac53aecf
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-27 08:45:01 +0700

    Update 2023-09-27 #5

--- dmt/utils/jsonqueue.js
+++ dmt/utils/jsonqueue.js
@@ -1,21 +1,47 @@
 const path = require('path');
+const fs = require('fs');
 const rootPath = process.env['ROOT_PATHS'];
 const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
 const moment = require('moment')
+
+// Recursive function to get files
+function getFiles(dir, recursive = false) {
+    let files = []
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true })
+    }
+    // Get an array of all files and directories in the passed directory using fs.readdirSync
+    const fileList = fs.readdirSync(dir)
+    // Create the full path of the file/directory by concatenating the passed directory and file/directory name
+    for (const file of fileList) {
+      const name = `${dir}/${file}`;
+      // Check if the current file/directory is a directory using fs.statSync
+      if (recursive && fs.statSync(name).isDirectory()) {
+        // If it is a directory, recursively call the getFiles function with the directory path and the files array
+        let childs = getFiles(name, files);
+        files.push(...childs)
+      } else {
+        // If it is a file, push the full path to the files array
+        files.push(name);
+      }
+    }
+    return files;
+  }
 
 class JsonQueue {
     //#region  Constructor
 
     constructor(path) {
         this._path = path
+        this._processing = false
     }
 
     //#endregion
 
     //#region Public Methods
 
-    processJson() {
-
+    processJson(fileName) {
+        console.log('process file: ' + fileName)
     } 
 
     writeFile(fileName, pObj) {
@@ -32,6 +58,13 @@ class JsonQueue {
 
     moveToError(fileName) {
         
+    }
+
+    processFiles() {
+        let files = getFiles(this._path, false)
+        if (files && files.length > 0) {
+            processJson()
+        }
     }
 
     //#endregion
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const WebServer = require('./nlib/nlib-express');
 // init logger
 const logger = require('./nlib/nlib-logger').logger;
 
-const fileSyncService = require('./dmt/filesync/filesync');
-const SFTPService = require('./dmt/ftp/nftp');
+const JsonQueue = require('./dmt/utils/jsonqueue').JsonQueue;
+
+//const fileSyncService = require('./dmt/filesync/filesync');
+//const SFTPService = require('./dmt/ftp/nftp');
 
 // write app version to log
 logger.info('start TA Server v1.1.0 build 400 update 2023-09-26 14:30');
@@ -16,3 +18,5 @@ wsvr.listen();
 
 //fileSyncService.start();
 //SFTPService.start();
+const reserveQueue = new JsonQueue(path.join('Queues', 'Reserve'))
+reserveQueue.processFiles()
